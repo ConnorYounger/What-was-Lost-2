637f60e1278bae170c296930eefc5d287cb41444
cameron-kent-sae <1020666@student.sae.edu.au>
2021-03-24 22:29:39 +1100

    updated beach spawner

--- What was lost game/Assets/Scripts/BeachObjectPopulator.cs
+++ What was lost game/Assets/Scripts/BeachObjectPopulator.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class BeachObjectPopulator : MonoBehaviour
 {
     public List<GameObject> beachObjects = new List<GameObject>();
-    private List<Transform> spawnPoints = new List<Transform>();
+    public List<Transform> spawnPoints = new List<Transform>();
 
     private void Start()
     {
+        // Add all child objects to list of spawn points
         foreach (Transform child in transform)
         {
             spawnPoints.Add(child);
         }
+
+        // Create an array of prefabs from the resource directory and then add each to the list of beach objects
+        GameObject[] objs = Resources.LoadAll<GameObject>("Beach Groups/");
+                
+        foreach (GameObject i in objs)
+        {
+            beachObjects.Add(i);
+        }
+
+        SpawnBeachObjects();
     }
 
-    private void SpawnBeachObject()
+    private void SpawnBeachObjects()
     {
+        for (int i =0; i < spawnPoints.Count; i++)
+        {
+            //int spawnNum = Random.Range(0, spawnPoints.Count);
+            int objNum = Random.Range(0, beachObjects.Count);
 
+            Instantiate(beachObjects[objNum], spawnPoints[i].position, Quaternion.identity);
+            //spawnPoints.RemoveAt(i);
+        }
     }
 }
