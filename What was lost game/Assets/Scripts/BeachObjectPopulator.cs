985737768cae8e2928b24a9fc2f153c22ecfdf68
cameron-kent-sae <1020666@student.sae.edu.au>
2021-03-24 22:00:09 +1100

    update spawner

--- What was lost game/Assets/Scripts/BeachObjectPopulator.cs
+++ What was lost game/Assets/Scripts/BeachObjectPopulator.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class BeachObjectPopulator : MonoBehaviour
 {
     public List<GameObject> beachObjects = new List<GameObject>();
+    private List<Transform> spawnPoints = new List<Transform>();
 
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            spawnPoints.Add(child);
+        }
+    }
 
+    private void SpawnBeachObject()
+    {
+
+    }
 }
