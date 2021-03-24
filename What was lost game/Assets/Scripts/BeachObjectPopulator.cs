cc959bd9397a483a2b631eece3270252e5c7b89a
cameron-kent-sae <1020666@student.sae.edu.au>
2021-03-24 21:20:52 +1100

    spawntest

---
+++ What was lost game/Assets/Scripts/BeachObjectPopulator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachObjectPopulator : MonoBehaviour
+{
+    public List<GameObject> beachObjects = new List<GameObject>();
+
+
+}
