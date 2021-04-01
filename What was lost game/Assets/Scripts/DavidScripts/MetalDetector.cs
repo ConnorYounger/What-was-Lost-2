27a8a74094a5689c5780c7082595b72f1edfba05
David Strudwick <david.strudwick1301@gmail.com>
2021-04-01 21:34:00 +1100

    Added a nicked detector bool to work with AI enemy

--- What was lost game/Assets/Scripts/DavidScripts/MetalDetector.cs
+++ What was lost game/Assets/Scripts/DavidScripts/MetalDetector.cs
@@ -19,19 +19,41 @@ public class MetalDetector : MonoBehaviour
     public AudioSource mDTone;
     private float maxFreq = 0.05f;
     float timer;
+
+    public bool mDetector;
     void Start()
     {
         mDTone = GetComponent<AudioSource>();
     }
     void Update()
     {
-        //tracks distance between player and object, triggering a Collect when the player walks over the object
-        var currentPosition = transform.position;
-        currentPosition.y = target.position.y;
-        distance = Vector3.Distance(currentPosition, target.position);
-        signalStrength.fillAmount = (1.0f - (distance / 120));
-        //print("distance = " + distance); //(print distance from current object to console) //- debug
-        
+        if (mDetector == true)
+        {
+
+            //tracks distance between player and object, triggering a Collect when the player walks over the object
+            var currentPosition = transform.position;
+            currentPosition.y = target.position.y;
+            distance = Vector3.Distance(currentPosition, target.position);
+            signalStrength.fillAmount = (1.0f - (distance / 120));
+            //print("distance = " + distance); //(print distance from current object to console) //- debug
+
+
+
+
+            //Metal Detector Sound system
+            timer += Time.deltaTime / distance;
+            if (timer > maxFreq && distance > 2)
+            {
+                mDClick.PlayOneShot(mDClick.clip, 1);
+                timer = 0;
+            }
+            if (distance < 2)
+            {
+                mDTone.volume = 1f;
+
+            }
+
+        }
         if (Input.GetKeyDown("e"))
         {
             if (distance < 2)
@@ -52,25 +74,19 @@ public class MetalDetector : MonoBehaviour
                 StartCoroutine(Refresh());
             }
         }
-
-        //Metal Detector Sound system
-        timer += Time.deltaTime / distance;
-        if (timer > maxFreq && distance > 2)
-        {
-            mDClick.PlayOneShot(mDClick.clip, 1);
-            timer = 0;
-        }
-        if (distance < 2)
-        {
-            mDTone.volume = 1f;
-
-        }
-        if (distance > 2 )
+        if (mDetector == false)
         {
             mDTone.volume = 0f;
-
+        }
+            if (Input.GetKey("b"))
+        { 
+            mDetector = false;
         }
 
+        else
+        {
+            mDetector = true;
+        }
     }
 
     IEnumerator Refresh()
