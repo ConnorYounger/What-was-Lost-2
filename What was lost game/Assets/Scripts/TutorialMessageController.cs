68b6a5e4ef5471075ebae7413b57240619e0c133
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-05 14:50:39 +1000

    update tut controller

--- What was lost game/Assets/Scripts/TutorialMessageController.cs
+++ What was lost game/Assets/Scripts/TutorialMessageController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialMessageController : MonoBehaviour
 {
@@ -17,23 +18,68 @@ public class TutorialMessageController : MonoBehaviour
     public string oGoalMessage = "use detector to search \n for lost items";
     public string oFinishMessage = "the day will end at sundown \n or if you find all the items";
 
-
+    private GameObject mMessage, mObjective;
     private TMP_Text messageText, objectiveTitle, objectiveText;
 
     private void Start()
     {
+        mMessage = GameObject.Find("UI_Message");
+        mObjective = GameObject.Find("UI_Objective");
         messageText = GameObject.Find("MessageText").GetComponent<TMP_Text>();
         objectiveTitle = GameObject.Find("GoalTitle").GetComponent<TMP_Text>();
         objectiveText = GameObject.Find("GoalText").GetComponent<TMP_Text>();
     }
 
-    public void DisplayTutorialMessage()
+    private void Update()
     {
-
+        if (Input.GetKeyDown("1"))
+        {
+            DisplayTutorialMessage(tWalk);
+        } else if (Input.GetKeyDown("2"))
+        {
+            DisplayTutorialMessage(tLook);
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            DisplayTutorialMessage(tDig);
+        }
+        else if (Input.GetKeyDown("4"))
+        {
+            DisplayObjectiveMessage(oGoalTitle, oGoalMessage);
+        }
+        else if (Input.GetKeyDown("5"))
+        {
+            DisplayObjectiveMessage(oGoalTitle, oFinishMessage);
+        }
     }
 
-    public void DisplayObjectiveMessage()
+    public void DisplayTutorialMessage(string _text)
     {
+        if (!mMessage.GetComponent<Image>().enabled)
+        {
+            mMessage.GetComponent<Image>().enabled = true;
+            messageText.text = _text;
+        }
+        else if (mMessage.GetComponent<Image>().enabled)
+        {
+            mMessage.GetComponent<Image>().enabled = false;
+            messageText.text = "";
+        }
+    }
 
+    public void DisplayObjectiveMessage(string _title, string _text)
+    {
+        if (!mObjective.GetComponent<Image>().enabled)
+        {
+            mObjective.GetComponent<Image>().enabled = true;
+            objectiveTitle.text = _title;
+            objectiveText.text = _text;
+        }
+        else if (mObjective.GetComponent<Image>().enabled)
+        {
+            mObjective.GetComponent<Image>().enabled = false;
+            objectiveTitle.text = "";
+            objectiveText.text = "";
+        }
     }
 }
