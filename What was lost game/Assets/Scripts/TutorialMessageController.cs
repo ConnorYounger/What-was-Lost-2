df2e65a3a13d7b5a22a1ec42b38235c2a53ad61f
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-05 12:44:03 +1000

    Update TutorialMessageController.cs

--- What was lost game/Assets/Scripts/TutorialMessageController.cs
+++ What was lost game/Assets/Scripts/TutorialMessageController.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 
 public class TutorialMessageController : MonoBehaviour
 {
-    [HideInInspector]
+    [Header("Tutorial Messages")]
     public string tWalk = "use 'w,a,s,d' to walk \n hold 'shift' to run";
-    [HideInInspector]
-    public string tLook;
-    [HideInInspector]
-    public string tDig;
-    [HideInInspector]
-    public string tInteract;
-    [HideInInspector]
-    public string tInventory;
+    public string tLook = "use mouse to \n look around";
+    public string tDig = "press 'e' to dig \n up an item";
+    public string tInteract = "press 'f' \n to interact";
+    public string tInventory = "press 'i' \n to view inventory";
+
+    [Header("Objective Messages")]
+    public string oGoalTitle = "today's goal";
+    public string oGoalMessage = "use detector to search \n for lost items";
+    public string oFinishMessage = "the day will end at sundown \n or if you find all the items";
 
 
     private TMP_Text messageText;
