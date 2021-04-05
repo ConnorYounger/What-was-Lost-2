5c4cda828fa9e623e8db31812028e4092aef4fd0
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-05 12:28:54 +1000

    Update TutorialMessageController.cs

--- What was lost game/Assets/Scripts/TutorialMessageController.cs
+++ What was lost game/Assets/Scripts/TutorialMessageController.cs
@@ -1,8 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TutorialMessageController : MonoBehaviour
 {
+    [HideInInspector]
+    public string tWalk = "Use 'W,A,S,D' to walk \n Hold 'Shift' to Run";
+    [HideInInspector]
+    public string tLook;
+    [HideInInspector]
+    public string tDig;
+    [HideInInspector]
+    public string tInteract;
+    [HideInInspector]
+    public string tInventory;
 
+
+    private TMP_Text messageText;
+
+    private void Start()
+    {
+        messageText = GameObject.Find("UI_Message").GetComponentInChildren<TMP_Text>();
+
+        messageText.text = tWalk;
+    }
 }
