52a9b7e7061fb3d7fa7ca608da0835888e0966b7
David Strudwick <david.strudwick1301@gmail.com>
2021-04-13 01:13:09 +1000

    It just works

---
+++ What was lost game/Assets/Scripts/DavidScripts/MDObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MDObject : MonoBehaviour
+{
+    void Start()
+    {
+        Randomize();
+    }
+    public void Randomize()
+    {
+        //Randomize location of next item
+        Vector3 pos = transform.position;
+
+        pos.x = Random.Range(0f, 10f);
+        pos.y = 3;
+        pos.z = Random.Range(0f, 10f);
+
+        transform.position = pos;
+    }
+}
