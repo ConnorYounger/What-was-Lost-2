fd8b92a2e11a53ffc64e9a67f21e284a2b7fe5e0
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-19 20:35:37 +1000

    implemented main menu logic

---
+++ What was lost game/Assets/Scripts/HomeMenuControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HomeMenuControl : MonoBehaviour
+{
+    public Button playBtn, optionsBtn, exitBtn;
+    public string playScene, optionsScene;
+
+    private void Start()
+    {
+        playBtn.onClick.AddListener(PlayGame);
+        optionsBtn.onClick.AddListener(OptionsMenu);
+        exitBtn.onClick.AddListener(ExitGame);
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene(playScene);
+    }
+
+    private void OptionsMenu()
+    {
+        SceneManager.LoadScene(optionsScene);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
