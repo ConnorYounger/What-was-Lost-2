f006e4b5ada2bf6afc3cb6bb1cbb831d633068bc
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-20 10:07:04 +1000

    updated build

---
+++ What was lost game/Assets/Scripts/OptionsMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsMenuController : MonoBehaviour
+{
+    public Button audioBtn, displayBtn, controlsBtn, backBtn;
+    public string mainScene;
+
+    private void Start()
+    {
+        audioBtn.onClick.AddListener(OpenAudio);
+        displayBtn.onClick.AddListener(OpenDisplay);
+        controlsBtn.onClick.AddListener(OpenControls);
+        backBtn.onClick.AddListener(GoBack);
+    }
+
+    private void GoBack()
+    {
+        SceneManager.LoadScene(mainScene);
+    }
+
+    private void OpenControls()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OpenDisplay()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OpenAudio()
+    {
+        throw new NotImplementedException();
+    }
+}
