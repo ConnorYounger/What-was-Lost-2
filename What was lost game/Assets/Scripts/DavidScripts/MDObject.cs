93cda783ab7946351855a4aaae3f744c9a75ea9d
David Strudwick <david.strudwick1301@gmail.com>
2021-04-21 19:08:59 +1000

    Reworking of spawning + mound system
    Mound rising is currently non functional,
    But spawning has been reqorked for 2 benefits:
    Prevention of the majority of valid spawns being on the navmesh shore border
    Streamlined workflow for importing to new maps

--- What was lost game/Assets/Scripts/DavidScripts/MDObject.cs
+++ What was lost game/Assets/Scripts/DavidScripts/MDObject.cs
@@ -6,21 +6,31 @@ using UnityEngine.AI;
 public class MDObject : MonoBehaviour
 {
     public Transform Player;
-    private float distance;
-    private float start;
-    private float offset;
-    private bool ready;
+    public float distance;
+    public GameObject[] spawnCoords;
     void Start()
     {
         Vector3 pos = transform.position;
 
-        pos.x = Random.Range(-117f, 33f);
+        /* pos.x = Random.Range(-117f, 33f);
+         pos.y = 3;
+         pos.z = Random.Range(-84f, 107f);*/
+        //Populate spawnCoords with EGOs at 2 corners of the spawn area, NW/SE (-x +z/+x -z)
+        var left = spawnCoords[0].transform.position.x;
+        var right = spawnCoords[1].transform.position.x;
+        var up = spawnCoords[0].transform.position.z;
+        var down = spawnCoords[1].transform.position.z;
+        print(left + down + up + right);
+        pos.x = Random.Range(left,right);
+        print(pos.x);
         pos.y = 3;
-        pos.z = Random.Range(-84f, 107f);
-
+        pos.z = Random.Range(up,down);
+        print(pos.z);
+        
         transform.position = pos;
+
         NavMeshHit hitloc;
-        if (NavMesh.SamplePosition(transform.position, out hitloc, 300f, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(transform.position, out hitloc, 10f, NavMesh.AllAreas))
         {
             pos = hitloc.position;
             transform.position = pos;
@@ -30,8 +40,6 @@ public class MDObject : MonoBehaviour
         {
             StartCoroutine("Move");
         }
-
-        start = transform.position.y;
     }
     IEnumerator Move()
     {
@@ -39,40 +47,47 @@ public class MDObject : MonoBehaviour
         yield return new WaitForSeconds(.1f);
         Vector3 pos = transform.position;
 
-        pos.x = Random.Range(-117f, 33f);
+        /* pos.x = Random.Range(-117f, 33f);
+         pos.y = 3;
+         pos.z = Random.Range(-84f, 107f);*/
+        //Populate spawnCoords with 2 corners of the spawn area, NW/SE (-x +z/+x -z)
+        var left = spawnCoords[0].transform.position.x;
+        var right = spawnCoords[1].transform.position.x;
+        var up = spawnCoords[0].transform.position.z;
+        var down = spawnCoords[1].transform.position.z;
+        pos.x = Random.Range(left, right);
         pos.y = 3;
-        pos.z = Random.Range(-84f, 107f);
-
+        pos.z = Random.Range(up, down);
+        print(pos.x + pos.z + "Move");
         transform.position = pos;
+
         NavMeshHit hitloc;
-        if (NavMesh.SamplePosition(transform.position, out hitloc, 300f, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(transform.position, out hitloc, 5f, NavMesh.AllAreas))
         {
             pos = hitloc.position;
             transform.position = pos;
-            ready = true;
+
         }
-        else 
-        { 
-            StartCoroutine("Move"); 
+        else
+        {
+            StartCoroutine("Move");
         }
-    
     }
         public void Update()
     {
-        distance = Vector3.Distance(transform.position, Player.position);
+       /* distance = Vector3.Distance(transform.position, Player.position);
         offset = (distance / 20f);
         if(ready = true)
         {
             transform.position = new Vector3(transform.position.x, (start - offset), transform.position.z);
         }
-        
-
+         
+        //Changed to separate script*/
         //Randomize location of next item
         if (Input.GetKeyDown("e"))
         {
             if (distance < 2)
             {
-                ready = false;
                 StartCoroutine("Move");
             }
             
