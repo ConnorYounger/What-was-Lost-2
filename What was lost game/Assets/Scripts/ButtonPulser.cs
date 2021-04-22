e5734870c7d33fc7d37d99d22677d6ab8f0bbaf6
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-22 22:21:43 +1000

    Level selector

---
+++ What was lost game/Assets/Scripts/ButtonPulser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonPulser : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("Grow parameters")]
+    public float approachSpeed = 0.005f;
+    public float growthBound = 1.1f;
+    public float shrinkBound = 0.8f;
+    private float currentRatio = 1;
+    private bool keepGoing = true;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StartCoroutine("StartPulsing");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopCoroutine("StartPulsing");
+        transform.localScale = Vector3.one;
+    }
+
+
+    public IEnumerator StartPulsing()
+    {
+        while (keepGoing)
+        {
+            // Get bigger for a few seconds
+            while (this.currentRatio != this.growthBound)
+            {
+                currentRatio = Mathf.Lerp(currentRatio, growthBound, approachSpeed);
+                transform.localScale = Vector3.one * currentRatio;
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            while (this.currentRatio != this.shrinkBound)
+            {
+                currentRatio = Mathf.Lerp(currentRatio, shrinkBound, approachSpeed);
+                transform.localScale = Vector3.one * currentRatio;
+
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
---
+++ What was lost game/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    [Header("Postcard Buttons")]
+    public Button level1Btn;
+    public Button level2Btn;
+    public Button level3Btn;
+    public Button level4Btn;
+    public Button level5Btn;
+    [Header("Levels Unlocked")]
+    public int levelsUnlocked;
+    [Header("Level Scenes to load")]
+    public string level1Scene;
+    public string level2Scene;
+    public string level3Scene;
+    public string level4Scene;
+    public string level5Scene;
+
+    private void Start()
+    {
+        UpdateDestinations();
+        level1Btn.onClick.AddListener(delegate { LoadDestination(level1Scene); });
+        level2Btn.onClick.AddListener(delegate { LoadDestination(level2Scene); });
+        level3Btn.onClick.AddListener(delegate { LoadDestination(level3Scene); });
+        level4Btn.onClick.AddListener(delegate { LoadDestination(level4Scene); });
+        level5Btn.onClick.AddListener(delegate { LoadDestination(level5Scene); });
+    }
+
+    private void Update()
+    {
+        UpdateDestinations();
+    }
+
+    private void UpdateDestinations()
+    {
+        levelsUnlocked = PlayerPrefs.GetInt("LevelUnlocked");
+        level3Btn.interactable = false;
+        level4Btn.interactable = false;
+        level5Btn.interactable = false;
+
+        switch (levelsUnlocked)
+        {
+            case 1:
+                level3Btn.interactable = true;
+                break;
+            case 2:
+                level3Btn.interactable = true;
+                level4Btn.interactable = true;
+                break;
+            case 3:
+                level3Btn.interactable = true;
+                level4Btn.interactable = true;
+                level5Btn.interactable = true;
+                break;
+        }
+
+    }
+    public void ResetPlayerPrefs()
+    {
+        level3Btn.interactable = false;
+        level4Btn.interactable = false; 
+        level5Btn.interactable = false;
+        PlayerPrefs.DeleteAll();
+    }
+
+    private void LoadDestination(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
