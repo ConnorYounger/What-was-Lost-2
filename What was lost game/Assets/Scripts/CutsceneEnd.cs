db567a34be5f9275f48eacba3755e9c9f14a6311
ConnorYounger <72112139+ConnorYounger@users.noreply.github.com>
2021-04-27 13:28:54 +1000

    Cutscene

---
+++ What was lost game/Assets/Scripts/CutsceneEnd.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class CutsceneEnd : MonoBehaviour
+{
+    public VideoPlayer video;
+    public string startingScene = "Concealed Cove";
+
+    private bool checkForVideo;
+
+    void Start()
+    {
+        Invoke("LoadNextLevel", 1);
+    }
+
+    void LoadNextLevel()
+    {
+        checkForVideo = true;
+    }
+
+    void Update()
+    {
+        if (checkForVideo && !video.isPlaying)
+        {
+            SceneManager.LoadScene(startingScene);
+        }
+    }
+}
