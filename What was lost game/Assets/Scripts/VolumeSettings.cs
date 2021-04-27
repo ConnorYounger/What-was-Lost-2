337899036ee82b3ac1912cb73fb47a9b4c583317
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-27 11:49:44 +1000

    a

---
+++ What was lost game/Assets/Scripts/VolumeSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSettings : MonoBehaviour
+{
+    public AudioMixer volumeMixer, musicMixer;
+
+    public void SetVolume(float volume)
+    {
+        volumeMixer.SetFloat("volume", volume);
+    }
+
+    public void SetMusic(float volume)
+    {
+        musicMixer.SetFloat("music", volume);
+
+    }
+}
