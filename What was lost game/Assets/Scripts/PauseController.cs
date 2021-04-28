eb096f31865e40cdcbbb6d1e33854197abe10388
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-29 09:32:55 +1000

    update pause

--- What was lost game/Assets/Scripts/PauseController.cs
+++ What was lost game/Assets/Scripts/PauseController.cs
@@ -8,13 +8,15 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class PauseController : MonoBehaviour
 {
     public GameObject optionCanvas;
-    public Button resumeBtn, optionsBtn, exitBtn;
+    public Button resumeBtn, optionsBtn, levelSelectBtn, exitBtn;
     public bool gamePaused;
     
     public GameObject player;
 
     [SerializeField]
     private string mainMenuScene = "Home Menu";
+    [SerializeField]
+    private string levelSelectScene = "LevelSelect";
 
     private void Start()
     {
@@ -26,6 +28,7 @@ public class PauseController : MonoBehaviour
 
         resumeBtn.onClick.AddListener(ResumeGame);
         optionsBtn.onClick.AddListener(OpenOptionsMenu);
+        levelSelectBtn.onClick.AddListener(LoadLevelSelect);
         exitBtn.onClick.AddListener(ExitToMain);
     }
 
@@ -74,6 +77,11 @@ public class PauseController : MonoBehaviour
     {
         GetComponent<Canvas>().enabled = false;
         optionCanvas.GetComponent<Canvas>().enabled = true;
+    }
+
+    private void LoadLevelSelect()
+    {
+        SceneManager.LoadScene(levelSelectScene);
     }
 
     private void ExitToMain()
