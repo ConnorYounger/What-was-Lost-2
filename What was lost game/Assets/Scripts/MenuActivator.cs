2d30bacf15cea6e72099533ce75dff45646a8db2
cameron-kent-sae <1020666@student.sae.edu.au>
2021-04-29 15:47:53 +1000

    build fix?!

---
+++ What was lost game/Assets/Scripts/MenuActivator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActivator : MonoBehaviour
+{
+    public GameObject menuCanvas, optionsCanvas;
+
+    private void Start()
+    {
+        menuCanvas.SetActive(true);
+        optionsCanvas.SetActive(true);
+
+    }
+}
